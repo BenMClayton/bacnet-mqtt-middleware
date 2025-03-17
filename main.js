baeb71a8f0f4268bf9e52f0cd047c4421bf11912
Ben Clayton <ben.m.clayton.2003@gmail.com>
2025-03-17 19:39:58 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Bacnet = require('bacstack');
 const client = new Bacnet({
 	apduTimeout: 10000,
-	interface: '192.168.0.246', // Your local IP on the same subnet as YABE
+	interface: '192.168.0.244', // Your local IP on the same subnet as YABE
 });
 
 
@@ -11,18 +11,18 @@ client.whoIs();
 client.on('iAm', (device) => {
 	console.log('Discovered device:', device);
 	// Use the discovered device’s address (which should include the proper port)
-	client.readProperty(
-		'192.168.1.105:52123',
-		{ type: Bacnet.enum.ObjectType.DEVICE, instance: 24 },
-		Bacnet.enum.PropertyIdentifier.objectList,
-		(err, value) => {
-			if (err) {
-				console.error('Direct read error:', err);
-			} else {
-				console.log('Object list:', JSON.stringify(value, null, 2));
-			}
-		}
-	);
+	// client.readProperty(
+	// 	'192.168.0.246',
+	// 	{ type: Bacnet.enum.ObjectType.DEVICE, instance: 24 },
+	// 	Bacnet.enum.PropertyIdentifier.objectList,
+	// 	(err, value) => {
+	// 		if (err) {
+	// 			console.error('Direct read error:', err);
+	// 		} else {
+	// 			console.log('Object list:', JSON.stringify(value, null, 2));
+	// 		}
+	// 	}
+	// );
 
 });
 
