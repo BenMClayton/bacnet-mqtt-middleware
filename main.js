cf3df12f787b6a8babdc2864d6cae09e1be68b52
BenMClayton <ben.m.clayton.2003@gmail.com>
2025-03-17 19:26:34 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 const Bacnet = require('bacstack');
 const client = new Bacnet({
 	apduTimeout: 10000,
-	interface: '192.168.1.105', // Your local IP on the same subnet as YABE
-  });
-  
+	interface: '192.168.0.246', // Your local IP on the same subnet as YABE
+});
+
 
 // Broadcast a Who-Is request so the simulator replies with I-Am
 client.whoIs();
 
 client.on('iAm', (device) => {
-  console.log('Discovered device:', device);
-  // Use the discovered device’s address (which should include the proper port)
-  client.readProperty(
-	'192.168.1.105:52123',
-	{ type: Bacnet.enum.ObjectType.DEVICE, instance: 24 },
-	Bacnet.enum.PropertyIdentifier.objectList,
-	(err, value) => {
-	  if (err) {
-		console.error('Direct read error:', err);
-	  } else {
-		console.log('Object list:', JSON.stringify(value, null, 2));
-	  }
-	}
-  );
-  
+	console.log('Discovered device:', device);
+	// Use the discovered device’s address (which should include the proper port)
+	client.readProperty(
+		'192.168.1.105:52123',
+		{ type: Bacnet.enum.ObjectType.DEVICE, instance: 24 },
+		Bacnet.enum.PropertyIdentifier.objectList,
+		(err, value) => {
+			if (err) {
+				console.error('Direct read error:', err);
+			} else {
+				console.log('Object list:', JSON.stringify(value, null, 2));
+			}
+		}
+	);
+
 });
 
 // Close the client after 20 seconds
 setTimeout(() => {
-  client.close();
-  console.log('Client closed.');
+	client.close();
+	console.log('Client closed.');
 }, 20000);
